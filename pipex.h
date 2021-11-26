0b58468f1a7da251034485bdbb3ce0487499c29e
Adam MARONI <amaroni@e2r12p13.clusters.42paris.fr>
2021-11-26 19:12:41 +0100

    Maj de parser.c avec ajout de la fonction ft_strjoin_plus_spaces

--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: amaroni <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/26 18:28:26 by amaroni           #+#    #+#             */
-/*   Updated: 2021/11/26 18:29:05 by amaroni          ###   ########.fr       */
+/*   Updated: 2021/11/26 18:59:02 by amaroni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,7 @@
 # include <unistd.h>
 # include "libft/libft.h"
 
+/* parser.c */
+char	*ft_strjoin_plus_spaces(int tabsize, char **tab);
 char	*ft_parse_input(int argc, char **argv);
 #endif
