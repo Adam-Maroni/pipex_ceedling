e1351e544d8907c24e44576cfadfc4caa64ded26
Adam MARONI <amaroni@e2r12p13.clusters.42paris.fr>
2021-11-26 18:42:47 +0100

    MAJ des tests pour parser.c

--- pipex.h
+++ pipex.h
@@ -1,9 +1,21 @@
-#ifndef PIPEX_H
-#define PIPEX_H
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-#include "libft/libft.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amaroni <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/26 18:28:26 by amaroni           #+#    #+#             */
+/*   Updated: 2021/11/26 18:29:05 by amaroni          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-char    *ft_parse_input(int argc, char **argv);
+#ifndef PIPEX_H
+# define PIPEX_H
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include "libft/libft.h"
+
+char	*ft_parse_input(int argc, char **argv);
 #endif
--- src/parser.c
+++ src/parser.c
@@ -1,32 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amaroni <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/26 18:26:40 by amaroni           #+#    #+#             */
+/*   Updated: 2021/11/26 18:38:00 by amaroni          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "parser.h"
 
-
-
-char    *ft_parse_input(int argc, char **argv)
+/* argc = nb of string in argv tab
+ * if argc is not 5 then return NULL to indicate an error.
+ * Otherwise concatenate the input (and spaced the element)
+ * and return that new concatenated string
+ */
+char	*ft_parse_input(int argc, char **argv)
 {
-    size_t  i;
-    size_t  total_strlen_of_args;
-    char *concat_str;
+	size_t	i;
+	size_t	total_strlen_of_args;
+	char	*concatenate_args;
 
-    if (argc != 5)
-    {
-        printf("Not the right number of arguments\n");
-        return (NULL);
-    }
-    i = 0;
-    total_strlen_of_args = 0;
-    while (++i < argc)
-    {
-        printf("ARGV[%d] = %s\n", i, argv[i]);
-        total_strlen_of_args += ft_strlen(argv[i]) + 1;
-    }
-        /*
-    concat_str = (char*)ft_calloc(sizeof(char), total_strlen_of_args);
-    i = 0;
-    while (++i < argc)
-        ft_strlcat(concat_str, argv[i], ft_strlen(concat_str) + ft_strlen(argv[i]) + 1);
-    return (concat_str);
-    */
-   printf("TOTAL_STRLEN_OF_ARGS = %d\n", total_strlen_of_args);
-    return ("Something");
+	if (argc != 5)
+	{
+		printf("Not the right number of arguments\n");
+		return (NULL);
+	}
+	total_strlen_of_args = 0;
+	i = 0;
+	while (++i < argc)
+		total_strlen_of_args += ft_strlen(argv[i]) + 1;
+	concatenate_args = (char *)ft_calloc(sizeof(char), total_strlen_of_args);
+	i = 0;
+	while (++i < argc)
+	{
+		ft_strlcat(concatenate_args, argv[i],
+			ft_strlen(concatenate_args) + ft_strlen(argv[i]) + 1);
+		if (i < argc - 1)
+			ft_strlcat(concatenate_args, " ", ft_strlen(concatenate_args) + 2);
+	}
+	return (concatenate_args);
 }
